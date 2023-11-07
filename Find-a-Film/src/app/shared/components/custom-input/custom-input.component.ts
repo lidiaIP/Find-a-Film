ed1691e62aa5391296a313e2c30e7b885b416e0c
lidia ibarra <kmh.mine@gmail.com>
2023-11-07 00:33:52 -0600

    033

--- Find-a-Film/src/app/shared/components/custom-input/custom-input.component.ts
+++ Find-a-Film/src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-input',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./custom-input.component.scss'],
 })
 export class CustomInputComponent  implements OnInit {
+ 
+  
 
   constructor() { }
 
