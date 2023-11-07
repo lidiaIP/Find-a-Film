83643cbda9ed1269aaf83afcc655608e163464c5
lidia ibarra <kmh.mine@gmail.com>
2023-11-07 12:16:36 -0600

    vrf

--- Find-a-Film/src/app/services/utils.service.ts
+++ Find-a-Film/src/app/services/utils.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
+import { LoadingController, ToastController, ToastOptions } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +8,19 @@ export class UtilsService {
 
   loadingCtrl = inject(LoadingController);
 
+  toastCtrl = inject(ToastController);
+
+
   // === loading === //
   loading() {
     return this.loadingCtrl.create({ spinner: 'crescent'})
+  }
+
+  // === toast === 
+
+  async presentToast(opts?: ToastOptions) {
+    const toast = await this.toastCtrl.create(opts);
+    toast.present();
   }
 
 }
