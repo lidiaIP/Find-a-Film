60b0d66fd8e7d0005aa30525f5b5f2c93e86e78f
lidia ibarra <kmh.mine@gmail.com>
2023-11-07 04:07:02 -0600

    fb

--- Find-a-Film/src/environments/environment.prod.ts
+++ Find-a-Film/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCtqQqjmRRrNkGVJR099Chv1BqOsiAArs0",
+    authDomain: "find-a-film-app.firebaseapp.com",
+    projectId: "find-a-film-app",
+    storageBucket: "find-a-film-app.appspot.com",
+    messagingSenderId: "485739987068",
+    appId: "1:485739987068:web:b3041adcd7a134a63849f9"
+  }
 };
--- Find-a-Film/src/environments/environment.ts
+++ Find-a-Film/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyCtqQqjmRRrNkGVJR099Chv1BqOsiAArs0",
+    authDomain: "find-a-film-app.firebaseapp.com",
+    projectId: "find-a-film-app",
+    storageBucket: "find-a-film-app.appspot.com",
+    messagingSenderId: "485739987068",
+    appId: "1:485739987068:web:b3041adcd7a134a63849f9"
+  }
 };
 
 /*
